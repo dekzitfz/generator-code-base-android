1a883d4af20e2c4ae78e405ff831d4617217b901
Adi Andrea <dekz.itfz@gmail.com>
2019-09-17 15:40:11 +0800

    create user interactions

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,11 +5,47 @@ module.exports = class extends Generator {
         super(args, opts);
     }
 
-    method1() {
-        this.log('method 1 just ran');
+    initializing(){
+        this.props = {};
+        this.log("initializing...");
     }
+
+    async prompting() {
+        this.answers = await this.prompt([
+          {
+            type: "input",
+            name: "name",
+            message: "What Is Your Application Name?",
+            default: this.appname // Default to current folder name
+          },
+          {
+            type: "input",
+            name: "package",
+            message: "Your Package Name",
+            default: "com.example.myapp"
+          },
+          {
+            type: "input",
+            name: "targetSDK",
+            message: "Your Target SDK",
+            default: 29
+          },
+          {
+            type: "input",
+            name: "minSDK",
+            message: "Your Minimum SDK",
+            default: 23
+          }
+        ]);
     
-    method2() {
-        this.log('method 2 just ran');
-    }
+        // this.log("app name", answers.name);
+        // this.log("cool feature", answers.cool);
+      }
+     
+      writing() {
+        this.log("app name", this.answers.name);
+        this.log("package name", this.answers.package);
+        this.log("target SDK", this.answers.targetSDK);
+        this.log("minimum SDK", this.answers.minSDK);
+      }
 };
