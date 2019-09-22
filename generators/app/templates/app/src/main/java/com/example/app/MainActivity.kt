4d51a23be505cb4577ab8c3dee440e5fa32a1b59
Adi Andrea <dekz.itfz@gmail.com>
2019-09-22 12:36:56 +0800

    update gradle with libs

--- generators/app/templates/app/src/main/java/com/example/app/MainActivity.kt
+++ generators/app/templates/app/src/main/java/com/example/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package <%= package %>
+package <%= package %> //<%= package %>
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- generators/app/templates/app/src/test/java/com/example/app/ExampleUnitTest.kt
+++ generators/app/templates/app/src/test/java/com/example/app/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.app
+package <%= package %>
 
 import org.junit.Test
 
