7e7d5c09dc8bd0180f11218bbfca625a6f6d26f7
Adi Andrea <dekz.itfz@gmail.com>
2019-10-04 15:39:36 +0800

    remove API level prompt

--- generators/app/index.js
+++ generators/app/index.js
@@ -24,23 +24,8 @@ module.exports = class extends Generator {
             name: "package",
             message: "Your Package Name",
             default: "com.example.myapp"
-          },
-          {
-            type: "input",
-            name: "targetSDK",
-            message: "Your Target SDK",
-            default: 29
-          },
-          {
-            type: "input",
-            name: "minSDK",
-            message: "Your Minimum SDK",
-            default: 23
           }
         ]);
-    
-        // this.log("app name", answers.name);
-        // this.log("cool feature", answers.cool);
       }
      
       writing() {
